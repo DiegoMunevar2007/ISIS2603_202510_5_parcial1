524e1e49d208913dea0eec14bd7f111e63fb4bb7
Diego Alejandro Munévar Pérez <d.munevar@uniandes.edu.co>
2025-03-09 11:59:14 -0500

    Mini cambio a RecordServiceTest

--- src/test/java/uniandes/dse/examen1/service/RecordServiceTest.java
+++ src/test/java/uniandes/dse/examen1/service/RecordServiceTest.java
@@ -1,6 +1,7 @@
 package uniandes.dse.examen1.service;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
@@ -15,6 +16,7 @@ import jakarta.transaction.Transactional;
 import uk.co.jemos.podam.api.PodamFactory;
 import uk.co.jemos.podam.api.PodamFactoryImpl;
 import uniandes.dse.examen1.entities.CourseEntity;
+import uniandes.dse.examen1.entities.RecordEntity;
 import uniandes.dse.examen1.entities.StudentEntity;
 import uniandes.dse.examen1.exceptions.RepeatedCourseException;
 import uniandes.dse.examen1.exceptions.RepeatedStudentException;
@@ -67,9 +69,13 @@ class RecordServiceTest {
     @Test
     void testCreateRecord() {
         assertDoesNotThrow(()->{
-            recordService.createRecord(login, courseCode, 5.0, courseCode);
+            recordService.createRecord(login, courseCode, 5.0, "4");
         });
+        RecordEntity record = studentRepository.findByLogin(login).get().getRecords().get(0);
         assertNotEquals(0, studentRepository.findByLogin(login).get().getRecords().size());
+        assertEquals(courseCode, record.getCourse().getCourseCode());   
+        assertEquals(5.0, record.getFinalGrade());
+        assertEquals("4", record.getSemester());
     }
 
     /**
@@ -79,13 +85,12 @@ class RecordServiceTest {
     void testCreateRecordMissingStudent() {
         try{
             recordService.createRecord("", courseCode, 5.0, courseCode);
-            fail("Debio haber lanzado una excepción");
-             }
-             catch (InvalidRecordException e){
+            fail("Debio haber lanzado una excepción porque el estudiante no existe");
+        }
+        catch (InvalidRecordException e){
 
-             }
-         }
-
+        }
+    }
     /**
      * Tests the creation of a record when the course code is wrong
      */
@@ -93,13 +98,13 @@ class RecordServiceTest {
     void testCreateInscripcionMissingCourse() {
         try{
             recordService.createRecord(login, null, 5.0, courseCode);
-            fail("Debio haber lanzado una excepción");
-             }
-             catch (InvalidRecordException e){
+            fail("Debio haber lanzado una excepción porque el curso no existe");
+            }
+            catch (InvalidRecordException e){
 
-             }
-         }
-
+            }
+        }
+    
     /**
      * Tests the creation of a record when the grade is not valid
      */
@@ -107,14 +112,14 @@ class RecordServiceTest {
     void testCreateInscripcionWrongGrade() {
         try{
             recordService.createRecord(login, courseCode, 0.0, courseCode);
-            fail("Debio haber lanzado una excepción");
+            fail("Debio haber lanzado una excepción porque no es una nota válida");
             }
         catch (InvalidRecordException e){
         }
 
         try{
             recordService.createRecord(login, courseCode, 5.1, courseCode);
-            fail("Debio haber lanzado una excepción");
+            fail("Debio haber lanzado una excepción porque no es una nota válida");
             }
         catch (InvalidRecordException e){
         }
@@ -129,7 +134,7 @@ class RecordServiceTest {
         try{
             recordService.createRecord(login, courseCode, 5.0, courseCode);
             recordService.createRecord(login, courseCode, 4.0, courseCode);
-            fail("Debio haber lanzado una excepción");
+            fail("Debio haber lanzado una excepción porque el estudiante ya pasó el curso");
         }
         catch (InvalidRecordException e){
         
